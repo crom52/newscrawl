7a4163893dfeadd9b0ef3b9b3fdf4ba721970f89
unknown <phuc52crom@gmail.com>
2022-06-17 01:46:54 +0700

    update

--- js/components/index.js
+++ js/components/index.js
@@ -1,6 +1,8 @@
 webix.ready(function () {
   Topbar.render();
+  PopupNewsReference.render();
   renderBody();
+  initEvent();
 });
 
 const renderBody = () => {
@@ -26,4 +28,15 @@ const renderBody = () => {
     ],
   };
   webix.ui(layout);
+};
+
+const initEvent = () => {
+  $(document).on('keydown', function (event) {
+    if (event.key == 'Escape') {
+      let popup = $$('popupNews');
+      if (popup && popup.isVisible()) {
+        popup.hide();
+      }
+    }
+  });
 };
